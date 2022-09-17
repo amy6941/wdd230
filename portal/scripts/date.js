623b46b9e88e04a2ac4e58ce35a61ef8d69d1d3b
Use your Github username <Enter your email address>
2022-09-17 13:16:50 -0600

    portal

---
+++ portal/scripts/date.js
@@ -0,0 +1,11 @@
+let daynames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let today = new Date();
+let dayName = days[today.getDay()];
+let monthName = months[today.getMonth()];
+let year = today.getFullYear();
+
+let fullDate = dayName + ", " + monthName + " " + today.getDate() + ", " + year;
+
+document.getElementById("currentDate").textContent = fullDate;
