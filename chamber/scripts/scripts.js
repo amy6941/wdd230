321446491bcc34e5719b42a6f94cdecfbb9ea63e
Use your Github username <Enter your email address>
2022-10-07 23:15:02 -0600

    chamber home

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,9 @@
+date = new Date().getFullYear();
+document.querySelector("#year").innerHTML = date
+document.querySelector("#timestamp").innerHTML= document.lastModified;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
