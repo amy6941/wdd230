51520caac63883054cae393ba7c6b29c3d0bf5ad
Use your Github username <Enter your email address>
2022-11-16 22:17:47 -0700

    new computer

---
+++ lesson10/weatherapi.js
@@ -0,0 +1,23 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const apiURL = 
+    'https://api.openweathermap.org/data/2.5/weather?=Fairbanks&units=imperial&appid=1f0879c2c447c81609b342c8422cc169';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+        throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
