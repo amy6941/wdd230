2e97198303b16e950e019200dbda66a5dace9e8b
Use your Github username <Enter your email address>
2022-11-16 22:18:33 -0700

    weather

---
+++ lesson10/weather.js
@@ -0,0 +1,36 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temperature');
+const weatherIcon = document.querySelector('#weathericon');
+const captionDesc = document.querySelector('figcaption');
+
+//Get weather data from the API - openweathermap.org
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=a3e347778ff618bdbb0ae6a89737f76c";
+
+//function to get JSON
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      // console.log(data);
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
--- lesson10/weatherapi.js
+++ lesson10/weatherapi.js
@@ -3,7 +3,7 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 const apiURL = 
-    'https://api.openweathermap.org/data/2.5/weather?=Fairbanks&units=imperial&appid=1f0879c2c447c81609b342c8422cc169';
+    'https://api.openweathermap.org/data/2.5/weather?lat=64.8284442&lon=-147.8094281&appid=1f0879c2c447c81609b342c8422cc169';
 
 async function apiFetch() {
     try {
