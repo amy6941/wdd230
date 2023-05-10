dd0c73dc72712b8fc423dd4cd5375bd625910cf5
amy6941 <bur20091@byui.edu>
2023-05-09 19:55:33 -0500

    start of chamber page

--- chamber/scripts/menuBtn.js
+++ chamber/scripts/menuBtn.js
@@ -1,6 +1,6 @@
-const mainnav = document.querySelector('.navigation')
-const hambutton = document.querySelector('#menu');
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
-});
+function toggleMenu () {
+    document.getElementById('primaryNav').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
