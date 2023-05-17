e36e6c8826a8ce9db90047dd354cb98c414cfa5b
amy6941 <bur20091@byui.edu>
2023-05-16 21:35:37 -0500

    header

---
+++ lesson5/bom/scripts/bom.js
@@ -0,0 +1,12 @@
+const input = document.getElementById('favchap');
+
+const button = document.getElementById('button');
+
+const list = document.getElementById('list');
+
+button.addEventListener('click', addChapter() {
+    delete = document.createElement('button');
+
+});
+
+<img src="images/alignment.png" alt="Example of PARC:Alignment design" width="300"></img>
