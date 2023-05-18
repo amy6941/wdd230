e4f5eec2aca9f51c0bacc7c16786d21344ea0e36
amy6941 <bur20091@byui.edu>
2023-05-17 22:56:08 -0500

    BOM and

--- lesson5/bom/scripts/bom.js
+++ lesson5/bom/scripts/bom.js
@@ -1,12 +1,41 @@
-const input = document.getElementById('favchap');
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const list = document.getElementById("list");
 
-const button = document.getElementById('button');
 
-const list = document.getElementById('list');
+function addChapter() {
 
-button.addEventListener('click', addChapter() {
-    delete = document.createElement('button');
+    if (input.value != "") {
+        // create the list item
+        const listItem = document.createElement('li');
+        listItem.textContent = input.value;
 
-});
+        const inputText = document.createElement("new");
+        inputText.textContent = input.value;
 
-<img src="images/alignment.png" alt="Example of PARC:Alignment design" width="300"></img>
+        // create the delete button
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = "❌";
+
+
+        // append to list item
+        listItem.appendChild(inputText);
+        listItem.appendChild(deleteButton);
+
+        // append to list
+        list.appendChild(listItem);
+
+        // add listener
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(listItem);
+        })
+
+    }
+
+    input.focus();
+    input.value = "";
+    
+
+}
+
+button.addEventListener("click", addChapter);
