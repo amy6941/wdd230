8c5fc11ee22759764dd07d93c6b6274b91c82223
amy6941 <bur20091@byui.edu>
2023-05-23 22:18:59 -0500

    bom

--- lesson5/bom/scripts/bom.js
+++ lesson5/bom/scripts/bom.js
@@ -1,40 +1,33 @@
-const input = document.getElementById("input");
-const button = document.getElementById("button");
+const input = document.getElementById("favchap");
+const button = document.getElementById("add");
 const list = document.getElementById("list");
 
-
 function addChapter() {
+  if (input.value != "") {
+    // create the list item
+    const listItem = document.createElement("li");
+    listItem.textContent = input.value;
 
-    if (input.value != "") {
-        // create the list item
-        const listItem = document.createElement('li');
-        listItem.textContent = input.value;
+    // create the delete button
 
-        const inputText = document.createElement("new");
-        inputText.textContent = input.value;
+    const deleteButton = document.createElement("delete");
+    deleteButton.textContent = "❌";
 
-        // create the delete button
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = "❌";
+    // append to list item
+    listItem.appendChild(deleteButton);
+    list.appendChild(listItem);
 
-        // append to list item
-        listItem.appendChild(inputText);
-        listItem.appendChild(deleteButton);
+    // append to list
+    list.appendChild(listItem);
 
-        // append to list
-        list.appendChild(listItem);
+    // add listener
+    deleteButton.addEventListener("click", function () {
+      list.removeChild(listItem);
+    });
+  }
 
-        // add listener
-        deleteButton.addEventListener('click', function() {
-            list.removeChild(listItem);
-        })
-
-    }
-
-    input.focus();
-    input.value = "";
-    
-
+  input.focus();
+  input.value = "";
 }
 
 button.addEventListener("click", addChapter);
