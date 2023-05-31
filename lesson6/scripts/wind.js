9c528b8efa6ddf4f568e9e23ccf14c1d98df0361
amy6941 <bur20091@byui.edu>
2023-05-30 20:39:34 -0500

    contrast errors

--- lesson6/scripts/wind.js
+++ lesson6/scripts/wind.js
@@ -2,13 +2,16 @@ var temp = 40;
 var speed = 5;
 var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 
+
+var windChill; 
+
+if (temp <= 50 && speed >= 3) {
+     windChill = Math.round(35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+
+} else {
+    windChill = "N/A";
+}
+
 var windChill = Math.round(windChill);
 
-// if (temp <= 50 && speed >= 3) {
-//     windchill = windChill(temp, speed);
-//     windchill = `${windChill}&deg;F`;
-// } else {
-//     windChill = "N/A";
-// }
-  
 document.getElementById("feelslike").innerHTML = windChill;
---
+++ lesson7/scripts/menuBtn.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+
+  document.getElementById("navigation").classList.toggle("open");
+  console.log('it works!');
+}
+
+const x = document.getElementById("hamButton")
+x.onclick = toggleMenu;
+ 
