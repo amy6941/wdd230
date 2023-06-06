c758d69186b85f46776fc633231080aaee180297
amy6941 <bur20091@byui.edu>
2023-06-06 11:34:55 -0500

    converting pix

--- lesson7/scripts/lazyimg.js
+++ lesson7/scripts/lazyimg.js
@@ -4,7 +4,7 @@ const imagesToLoad = document.querySelectorAll("img[data-src]");
 // optional parameters being set for the IntersectionalObserver
 const imgOptions = {
     threshhold: 1,
-    rootMargin: "opx 0px 50px 0px"
+    rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
