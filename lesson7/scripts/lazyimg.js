f88445c5bdce695df086b797f2b600acbb13c8c4
amy6941 <bur20091@byui.edu>
2023-06-06 11:24:55 -0500

    placeholder image

--- lesson7/scripts/lazyimg.js
+++ lesson7/scripts/lazyimg.js
@@ -1,32 +1,88 @@
-const images = document.querySelectorAll("[data-src]");
+// get all imgs with data-src attribute
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
+// optional parameters being set for the IntersectionalObserver
+const imgOptions = {
+    threshhold: 1,
+    rootMargin: "opx 0px 50px 0px"
+};
 
-    img.src = src;
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+// first check to see if Intersection Observer is suported
+
+if('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+        });
+    }, imgOptions);
+
+    // lopp through each img and check status and load if necessary
+    imagesToLoad.forEach((img) => {
+        imgObserver.observe(img);
+      });
+}
+else { //load all images if not supported
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
 }
 
 
 
-const imgOptions = {
-    threshhold: 0,
-    rootMargin: "0px, 0px, 300px, 0px"
-};
 
-const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-       if (!entry.isIntersecting) {
-        return;
-       } else {
-         preloadImage(entry.target);
-         imgObserver.unobserve(entry.target);
-       }
-    })
-}, imgOptions);
 
-images.forEach(image => {
-    imgObserver.observe(image);
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const images = document.querySelectorAll("[data-src]");
+
+// function preloadImage(img) {
+//     const src = img.getAttribute("data-src");
+//     if(!src) { 
+//         return;
+//     }
+
+//     img.src = src;
+// }
+
+
+
+// const imgOptions = {
+//     threshhold: 0,
+//     rootMargin: "0px, 0px, 300px, 0px"
+// };
+
+// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+//     entries.forEach(entry => {
+//        if (!entry.isIntersecting) {
+//         return;
+//        } else {
+//          preloadImage(entry.target);
+//          imgObserver.unobserve(entry.target);
+//        }
+//     })
+// }, imgOptions);
+
+// images.forEach(image => {
+//     imgObserver.observe(image);
+// });
