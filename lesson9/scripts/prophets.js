ca1219a1378077e0df2a17e5af93f85602d0f786
amy6941 <bur20091@byui.edu>
2023-06-13 21:39:58 -0500

    new colors prophets

--- lesson9/scripts/prophets.js
+++ lesson9/scripts/prophets.js
@@ -17,13 +17,13 @@ const displayProphets = (prophets) => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
-        let h3 = document.createElement('h3');
-        let p = document.createElement('p');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
 
         //build the h2 content out to show the prophets full name - finish the template string
         h2.textContent = `${prophet.name} ${prophet.lastname}`;
-        h3.textContent = `Birthdate: ${prophet.birthdate}`;
-        p.textContent = `Birth place: ${prophet.birthplace}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
 
         //build the image portrait by setting all the relevant attributes
         portrait.setAttribute('src', prophet.imageurl);
@@ -34,11 +34,11 @@ const displayProphets = (prophets) => {
 
         //append the section(card) with the createds elements
         card.appendChild(h2);
-        card.appendChild(h3);
-        card.appendChild(p);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
         card.appendChild(portrait);
         cards.appendChild(card);
-        
+
     }) //end of forEach loop
         
 } // end of function
