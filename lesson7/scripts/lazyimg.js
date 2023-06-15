d1216576b253fd23f29c84f5255cfe7099338aba
amy6941 <bur20091@byui.edu>
2023-06-15 13:42:21 -0500

    lazy load lesson 7 fixed!

--- lesson7/scripts/lazyimg.js
+++ lesson7/scripts/lazyimg.js
@@ -1,91 +1,33 @@
-// get all imgs with data-src attribute
-const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-// optional parameters being set for the IntersectionalObserver
-const imgOptions = {
-    threshhold: 1,
-    rootMargin: "0px 0px 50px 0px"
-};
+const images = document.querySelectorAll("img[data-src]");
 
-const loadImages = (image) => {
-  image.setAttribute("src", image.getAttribute("data-src"));
-  image.onload = () => {
-    image.removeAttribute("data-src");
-  };
-};
-
-// first check to see if Intersection Observer is suported
-if('IntersectionObserver' in window) {
-    const Observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
-    }, imgOptions);
-
-    // lopp through each img and check status and load if necessary
-    imagesToLoad.forEach((img) => {
-        Observer.observe(img);
-      });
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    } else {
+        img.src = src;
+        img.removeAttribute("data-src");
+    }
 }
-else { //load all images if not supported
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
+
+const imgOptions = {
+    root: document.querySelector('#scrollArea'),
+    rootMargin: '10px',
+    threshold: 1.0
 };
 
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const images = document.querySelectorAll("[data-src]");
-
-// function preloadImage(img) {
-//     const src = img.getAttribute("data-src");
-//     if(!src) { 
-//         return;
-//     }
-
-//     img.src = src;
-// }
-
-
-
-// const imgOptions = {
-//     threshhold: 0,
-//     rootMargin: "0px, 0px, 300px, 0px"
-// };
-
-// const imgObserver = new IntersectionObserver((entries, imgObserver) => {
-//     entries.forEach(entry => {
-//        if (!entry.isIntersecting) {
-//         return;
-//        } else {
-//          preloadImage(entry.target);
-//          imgObserver.unobserve(entry.target);
-//        }
-//     })
-// }, imgOptions);
-
-// images.forEach(image => {
-//     imgObserver.observe(image);
-// });
+images.forEach(image => {
+    imgObserver.observe(image);
+})
--- lesson7/scripts/menuBtn.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-
-  document.getElementById("navigation").classList.toggle("open");
-  console.log('it works!');
-}
-
-const x = document.getElementById("hamButton")
-x.onclick = toggleMenu;
- 
