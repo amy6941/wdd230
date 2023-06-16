07c5d746c3ce88975397bb938c759617e50b07d9
amy6941 <bur20091@byui.edu>
2023-06-15 22:13:27 -0500

    portal fix

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,46 @@
+
+
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards');
+
+    prophets.forEach((prophet) => {
+        //Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
+
+        //build the h2 content out to show the prophets full name - finish the template string
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        //build the image portrait by setting all the relevant attributes
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        //append the section(card) with the createds elements
+        card.appendChild(h2);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+        cards.appendChild(card);
+
+    }) //end of forEach loop
+        
+} // end of function
