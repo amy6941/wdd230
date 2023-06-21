3d2a8cc4a7f25ef721cbfbbf1142f95f84d36396
amy6941 <bur20091@byui.edu>
2023-06-21 12:36:10 -0500

    weather fixes

--- chamber/scripts/weatherapi.js
+++ chamber/scripts/weatherapi.js
@@ -2,9 +2,9 @@
 const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
-const tempNow = document.querySelector('current');
-const windSpeed = document.querySelector('speed');
-const feel = document.querySelector('feelslike');
+const tempNow = document.querySelector('#current');
+const windSpeed = document.querySelector('#speed');
+const feel = document.querySelector('#feelslike');
 
 
 
@@ -27,16 +27,20 @@ async function apiFetch() {
   
   apiFetch();
 
+
   function  displayResults(weatherData) {
     currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
     tempNow.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
     windSpeed.innerHTML = `${weatherData.wind.speed.toFixed(0)}`;
-    feel.innerHTML = `${weatherData.wind.feels_like.toFixed(0)}`;
+    feel.innerHTML = `<strong>${weatherData.main.feels_like.toFixed(0)}</strong>`;
   
-    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
     const desc = weatherData.weather[0].description;
   
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
   }
+
+
+  
--- chamber/scripts/wind.js
+++ chamber/scripts/wind.js
@@ -1,5 +1,7 @@
-var temp = 40;
-var speed = 5;
+
+
+var temp = document.getElementById("current-temp");
+var speed = document.getElementById("speed");
 var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 
 
@@ -9,9 +11,9 @@ if (temp <= 50 && speed >= 3) {
      windChill = Math.round(35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 
 } else {
-    windChill = temp;
+    windChill = 'None';
 }
 
 var windChill = Math.round(windChill)  
 
-document.getElementById("feelslike").innerHTML = windChill;
+document.getElementById("chill").innerHTML = windChill;
