f80a24c778d78eef652c2797e0ce3f0316756e2b
amy6941 <bur20091@byui.edu>
2023-06-24 13:10:59 -0500

    working on windchill

--- chamber/scripts/wind.js
+++ chamber/scripts/wind.js
@@ -1,7 +1,7 @@
 
-var temp = document.getElementById("current-temp");
-var speed = document.getElementById("speed");
-var windChill = (35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+var temp = document.getElementById("#current-temp");
+var speed = document.getElementById("#speed");
+var windChill = "";
 
 
 var windChill; 
@@ -10,9 +10,9 @@ if (temp <= 50 && speed >= 3) {
      windChill = Math.round(35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
 
 } else {
-    windChill.textContent = 'None';
+    windChill.textContent = ("N/A");
 }
 
 var windChill = Math.round(windChill)  
 
-document.getElementById("chill").innerHTML = windChill;
+document.querySelector("#chill").innerHTML = windChill;
