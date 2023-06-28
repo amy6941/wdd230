e6bedd8dee76316f91ecf5288256210dcd15bf14
amy6941 <bur20091@byui.edu>
2023-06-28 17:21:40 -0500

    windchill

--- chamber/scripts/wind.js
+++ chamber/scripts/wind.js
@@ -1,18 +1,16 @@
 
-var temp = document.getElementById("#current-temp");
-var speed = document.getElementById("#speed");
-var windChill = "";
-
-
-var windChill; 
+const temp = document.getElementById("current-temp");
+const speed = document.getElementById("speed");
+const windC = document.getElementById("chill");
+let windChill = "";
+ 
 
 if (temp <= 50 && speed >= 3) {
-     windChill = Math.round(35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    windChill = Math.round(35.74 + (0.6215 * temp)) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    windC.textContent = windChill;
 
 } else {
-    windChill.textContent = ("N/A");
+    windC.textContent = "N/A";
 }
 
-var windChill = Math.round(windChill)  
 
-document.querySelector("#chill").innerHTML = windChill;
