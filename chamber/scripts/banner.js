287343899eee8623ea3d90d47b8ed0367619f732
amy6941 <bur20091@byui.edu>
2023-07-06 13:57:48 -0500

    updating banner days

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,7 +1,7 @@
 const today = new Date().getDay();
 const banner = document.getElementById("meeting");
 
-if (today === 0 || today === 1) {
+if (today === 1 || today === 2) {
     banner.style.display = "block";
 } else {
     banner.style.display = "none";
