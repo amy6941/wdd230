ed2d7ad66333c0ebfc33cbca8670ae9448bdd0e8
amy6941 <bur20091@byui.edu>
2023-07-18 23:40:04 -0500

    json errors

--- bountifulFoods/scripts/fruit.js
+++ bountifulFoods/scripts/fruit.js
@@ -1,28 +1,23 @@
-//const url = 'https://amy6941.github.io/wdd230/bountifulfoods/scripts/fruitdata.json'
+const url = 'https://amy6941.github.io/wdd230/bountifulfoods/scripts/data.json'
 
-const url = 'fruitdata.json'
+// const url = 'data.json';
 
-const fruit1 = document.getElementById("#fruit1");
-const fruit2 = document.getElementById('fruit2');
-const fruit3 = document.getElementById('fruit3');
+let fruit1 = document.getElementById('fruit1');
+let fruit2 = document.getElementById('fruit2');
+let fruit3 = document.getElementById('fruit3');
 
-const fruit = 1;
-const carbs = 2;
-const protein = 3;
-const fat = 4;
-const calories = 5;
-const sugar = 6;
 
-let fruits = null;
 
-fetch(url)
-.then(function  (response) {
-    return response.json();
-})
-.then(function(jsonObject) {
-    fruits = jsonObject;
-    fruits.forEach(loadfruitdata);
-});
+// let fruits = null;
+
+function getData() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.table(data);
+    })
+}
+getData();
 
 function loadfruitdata(fruit) {
     let newfruit1 = document.createElement('option');
@@ -34,3 +29,9 @@ function loadfruitdata(fruit) {
 
 }
 
+// const fruit = 1;
+// const carbs = 2;
+// const protein = 3;
+// const fat = 4;
+// const calories = 5;
+// const sugar = 6;
